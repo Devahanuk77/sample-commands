4473cb1881e572727de88e063ad53e443723b385
=Devahanuk77 <=>
2024-06-12 20:35:55 +0530

    added sample project to local repo

---
+++ src/main/java/com/dev/Sample/App.java
@@ -0,0 +1,19 @@
+package com.dev.Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void insertData(String data) {
+		System.out.println("Data ="+data);
+	}
+	
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+    }
+}
